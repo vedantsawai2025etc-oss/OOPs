21e83a6ee5b3f34bd4c3c721c7fd411e15aa4722
vedantsawai2025etc-oss <vedantsawai2025.etc@mmcoe.edu.in>
2026-09-23 10:14:52 +0530

    Update Pascal triangle .cpp

--- Practical 1 /Pascal triangle .cpp
+++ Practical 1 /Pascal triangle .cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+using namespace std;
 
+// Function to calculate the Binomial Coefficient
+int BinoCoef(int n, int k)
+{
+    int res = 1;  // Stores the result
+
+    // Calculate the Binomial Coefficient
+    for(int i = 0; i < k; i++)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+
+    return res;
+}
+
+// Function to print Pascal's Triangle
+void printPascal(int n)
+{
+    // Loop for each row
+    for(int l = 0; l < n; l++)
+    {
+        // Print elements in each row
+        for(int m = 0; m <= l; m++)
+        {
+            cout << BinoCoef(l, m) << "\t";
+        }
+        cout << endl;  // Move to next row
+    }
+}
+
+int main()
+{
+    int rows;
+
+    // Take number of rows from the user
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    // Print Pascal Triangle
+    printPascal(rows);
+
+    return 0; 
+}
